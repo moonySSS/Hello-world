546bc1f0d2bb7fab870193bd94d2d9be0e07c71b
moonySSS <32222330+moonySSS@users.noreply.github.com>
2017-10-31 13:04:11 +0800

    Add files via upload

---
+++ MAX.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	double a,b;
+	printf("ÇëÊäÈëa,bµÄÖµ\n");
+	scanf("%lf%lf",&a,&b);
+	void max();
+		void max(double a,double b);
+		if(a>b){ 
+		printf("MAX=%lf\n",a);
+		} else {
+		if(a!=b){ 
+		printf("MAX=%lf\n",b);
+		} else{
+		printf("a=b\n"); 
+		 }
+	 } 
+	 return 0;
+}
+
